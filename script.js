8a5620cf651eb558ad67998eab24d6aaebfbdf66
Aizuked <aizyk.tt@gmail.com>
2021-04-13 13:15:10 +0400

    html: radio->select, added script src; js:check text input

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function mainjsfunc() {
+    const digitcheck = /^\d+$/;
+    let n = document.getElementById("n").value;
+    let flag = document.getElementById("selector").value;
+    if (digitcheck.test(n) == 0) {
+        alert("Неверный ввод!");
+        return;
+    }
     
 }
