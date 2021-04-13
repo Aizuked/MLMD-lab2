ac8514193ce933904476b93681bf859c95cb1158
Aizuked <aizyk.tt@gmail.com>
2021-04-13 18:20:49 +0400

    finished

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function mainjsfunc() {
         }
     }
     if (refctr == 6) result += "рефлективная ";
+    else result += "антирефлективная ";
 
     //Симметричность
     var symmctr = 0;
@@ -28,11 +29,39 @@ function mainjsfunc() {
             }
         }
     }
-    if (symmctr == 1) result += "антисимметричная ";
+    if (symmctr > 0) result += "антисимметричная ";
     else result += "симметричная ";
 
     //Транзитивность
-    
+    var trans = 0;
+    for (let i = 0; i < 6; i++) {
+        for (let j = i + 1; j < 6; j++) {
+            if (matrix[i][j].checked && i != j) {
+                for (let jj = i + 2; jj < 6; jj++) {
+                    if (matrix[j][jj].checked && matrix[i][jj].checked) trans = 1;
+                    if (matrix[j][jj].checked && !matrix[i][jj].checked) {
+                        trans = -1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    for (let i = 5; i >= 0; i--) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (matrix[i][j].checked && i != j) {
+                for (let jj = i - 2; jj >= 0; jj--) {
+                    if (matrix[j][jj].checked && matrix[i][jj].checked) trans = 1;
+                    if (matrix[j][jj].checked && !matrix[i][jj].checked) {
+                        trans = -1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if (trans > 0) result += "транзитивная"
+    else result += "антитранзитивная"
 
     //Вывод
     document.getElementById("output").innerHTML = result;
