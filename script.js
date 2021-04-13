e040883b295daf0428692d6b8c97f02255f5a24e
Aizuked <aizyk.tt@gmail.com>
2021-04-13 15:52:36 +0400

    transitive lefto 2do

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 function mainjsfunc() {
-    const digitcheck = /^\d+$/;
-    let n = document.getElementById("n").value;
-    let flag = document.getElementById("selector").value;
-    if (digitcheck.test(n) == 0) {
-        alert("Неверный ввод!");
-        return;
+    var result = "";
+    //Создание и заполнение матрицы чекбоксов
+    var matrix = new Array(6);
+    for (let i = 0; i < 6; i++) matrix[i] = new Array(6);
+    for (let i = 1; i < 7; i++) {
+        for (let j = 1; j < 7; j++) {
+            matrix[i - 1][j - 1] = document.getElementById("cb" + i + j);
+        }
     }
+
+    //Рефлексивность
+    var refctr = 0;
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+            if (matrix[i][j].checked && i == j) refctr++;
+        }
+    }
+    if (refctr == 6) result += "рефлективная ";
+
+    //Симметричность
+    var symmctr = 0;
+    for (let i = 0; i < 6; i++) {
+        for (let j = i + 1; j < 6; j++) {
+            if (i != j && ((matrix[i][j].checked && !matrix[5 - i][5 - j].checked) || (!matrix[i][j].checked && matrix[5 - i][5 - j].checked))) {
+                symmctr++;
+                break;
+            }
+        }
+    }
+    if (symmctr == 1) result += "антисимметричная ";
+    else result += "симметричная ";
+
+    //Транзитивность
     
+
+    //Вывод
+    document.getElementById("output").innerHTML = result;
 }
